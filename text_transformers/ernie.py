255c5ee0fc500319907054e165841b555b7f6f10
Mikhail Makarov <makarov.mikhail11@gmail.com>
2021-02-06 09:31:39 +0300

    Add torch.no_grad

--- text_transformers/ernie.py
+++ text_transformers/ernie.py
@@ -24,20 +24,21 @@ class Ernie(BaseTextTransformer):
         # print(summary(self.model, input_size=(1000, 512)))
 
     # fake fit to be consistent with count and tfidf vectorizers usage
-    def fit_transform(self, texts, batch_size=50):
+    def fit_transform(self, texts, batch_size=16):
         # sentence_embeddings = self.model.encode(texts)
 
-        batch_embs = []
-        for text_batch in tqdm(iter_batches(texts.tolist(), batch_size)):
-            max_length = min(max([len(t.split(" ")) for t in text_batch]), 512)
-            tokenized = self.tokenizer(text_batch,
-                                       padding="max_length",
-                                       max_length=max_length,
-                                       truncation="only_first")["input_ids"]
-            tokenized = torch.tensor(tokenized).to(device)
-            out = self.model(tokenized)
-            embeddings_of_last_layer = out[0]
-            cls_embeddings = embeddings_of_last_layer[:,0,:].detach().cpu().numpy()
-            batch_embs.append(cls_embeddings)
+        with torch.no_grad():
+            batch_embs = []
+            for text_batch in tqdm(iter_batches(texts.tolist(), batch_size)):
+                max_length = min(max([len(t.split(" ")) for t in text_batch]), 512)
+                tokenized = self.tokenizer(text_batch,
+                                           padding="max_length",
+                                           max_length=max_length,
+                                           truncation="only_first")["input_ids"]
+                tokenized = torch.tensor(tokenized).to(device)
+                out = self.model(tokenized)
+                embeddings_of_last_layer = out[0]
+                embeddings = embeddings_of_last_layer.mean(axis=1)
+                batch_embs.append(embeddings)
 
         return np.vstack(batch_embs)
